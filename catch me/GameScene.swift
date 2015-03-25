5d3e9f8d8b396ce8e8a2070c0db6ace9759cc3ca
Cnnamon <taututis@yahoo.com>
2015-03-25 17:15:30 +0200

    lightening, bombs drawing, explosion and sparkling emitters

--- catch me/GameScene.swift
+++ catch me/GameScene.swift
@@ -18,6 +18,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         static let All          : UInt32 = UInt32.max
         static let Ground       : UInt32 = 0b1       // 1
         static let Hero         : UInt32 = 0b10      // 2
+        static let Bomb         : UInt32 = 0b100     // 4
     }
     
     struct ShadowCategory {
@@ -27,14 +28,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         static let Hero         : UInt32 = 0b10      // 2
     }
     
+    struct HeroStats {
+        static var Shield               : Int = 0   //time left on shield (or block hits)
+        static var Lifes                : Int = 1   //change in function
+        static var ExplosionTime        : Int = 0
+        static var ExplosionTimeDelay   : Int = 0
+        static var BombPower            : Int = 0
+        static var BombsCount           : Int = 0
+    }
+    
     // Main character
     let heroImage = SKTexture(imageNamed: "Hero.png")
-    var heroSprite : SKSpriteNode?
+    var heroSprite : SKSpriteNode!
     // Background
     var backgroundImagesArray = [SKTexture]()
     var backgroundArray = [SKSpriteNode]()
     // Terrain
     var groundArray = [SKSpriteNode]()
+    // Bomb
+    let bombImage = SKTexture(imageNamed: "Bomb.png")
+    var bombSprite : SKSpriteNode!
     // Moving and jumping objects
     var moveToRight = false
     var moveToLeft = false
@@ -63,6 +76,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var generatedWorldFromY: Int = 0
     // Light for hero
     var light = SKLightNode()
+    // Emiter for hero
+    var emiter = SKEmitterNode()
+    // Skaitiklis kiek bombu padeta
+    var bombsCounter: Int = 0
     
     override func didMoveToView(view: SKView) {
         
@@ -86,9 +103,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             self.overlay?.zPosition = 10
             addChild(overlay!)
             
-            
-            
-            
             //gravity
             physicsWorld.gravity = CGVectorMake(0, -5)
             
@@ -96,36 +110,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             physicsWorld.contactDelegate = self
             
             //creating ground
-            for(var i = 0; i < 10; i++) {
+            for(var i = 0; i < 100; i++) {
                 var ground = SKSpriteNode(imageNamed: "Ground.png")
+                ground.xScale = 0.5
+                ground.yScale = 0.1
                 createGround(ground, view: view)
+                ground.shadowCastBitMask = ShadowCategory.Ground
+                ground.zPosition = 1
                 groundArray += [ground]
                 world!.addChild(ground)
             }
             
-            //creating main character
-            
+            // creating main character
             createHero(view)
-            
+            setHeroStats()
+            createLight(view)
+            heroSprite?.addChild(light)
             world!.addChild(heroSprite!)
             
-            //background color
+            // background color
             self.backgroundColor = UIColor.blackColor()
             
-            //background images added to array
+            // background images added to array
             for(var i = 0; i<5; i++){
                 let imageText = "Background-\(i).png"
                 let image = SKTexture(imageNamed: imageText)
                 backgroundImagesArray += [image]
             }
             
-            //createBackground(view)
+            createBackground(view)
             
             //creating light for character
             
-            createLight(view)
-            heroSprite?.addChild(light)
+            
         }
+    }
+    
+    func setHeroStats(){
+        HeroStats.ExplosionTimeDelay = 5
+        HeroStats.Lifes = 3
+        HeroStats.BombsCount = 1
+        HeroStats.ExplosionTime = 1
+        HeroStats.BombPower = 1
     }
     
     func createBackground(view: SKView){
@@ -136,24 +162,34 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let imageHeight = backgroundImagesArray[0].size().height
         
         var imageNumber = Int(arc4random_uniform(count))
-        var brick = SKSpriteNode(texture: backgroundImagesArray[imageNumber])
         
-        let maxX = heroSprite!.position.x + self.frame.width/2
-        let minX = heroSprite!.position.x - self.frame.width/2
-        let maxY = heroSprite!.position.y + self.frame.height/2
-        let minY = heroSprite!.position.y - self.frame.height/2
+        
+        
+        let maxX = heroSprite!.position.x + self.frame.width
+        let minX = heroSprite!.position.x - self.frame.width
+        let maxY = heroSprite!.position.y + self.frame.height
+        let minY = heroSprite!.position.y - self.frame.height
         
         var collumn = Int (((maxX - minX) + imageLengh/2) / imageLengh)
         var row = Int ((maxY - minY) / imageHeight)
         
         for(var i = 0 ; i<row; i++){
+            let index = imageLengh / 2
             for(var j = 0; j<collumn; j++){
-                backgroundArray += [brick]
+                var imageNumber = Int(arc4random_uniform(count))
+                var brick = SKSpriteNode(texture: backgroundImagesArray[imageNumber])
+                if(j % 2 == 0){
+                    brick.position.x = minX + (CGFloat(i) * imageLengh) + CGFloat(index)
+                }else{
+                    brick.position.x = minX + (CGFloat(i) * imageLengh)
+                }
+                brick.position.y = minY + CGFloat   (j) * imageHeight
+                brick.zPosition = 0
+                backgroundArray.append(brick)
             }
         }
-        
-        for br in backgroundArray{
-            world!.addChild(br)
+        for (var i = 0 ; i<backgroundArray.count; i++){
+            world!.addChild(backgroundArray[i])
         }
     }
     
@@ -178,19 +214,58 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         var touch = touches.anyObject() as UITouch!
         var touchLocation = touch.locationInNode(self)
-        
-        
         let x = countNormalx()
-        if touchLocation.x > x {
-            moveToRight = true
-        } else {
-            moveToLeft = true
+        let y = countNormaly()
+        let heroHalfWidth = heroSprite.frame.width / 2
+        let heroHalfHeight = heroSprite.frame.height / 2
+        
+        if(touchLocation.x > x - heroHalfWidth && touchLocation.x < x + heroHalfWidth && touchLocation.y > y - heroHalfHeight && touchLocation.y < y + heroHalfHeight){
+            if(HeroStats.BombsCount > bombsCounter){
+                dropBomb(heroSprite.position.x, y: heroSprite.position.y)
+                bombsCounter++
+            }
+        }else{
+            if touchLocation.x > x {
+                moveToRight = true
+            } else {
+                moveToLeft = true
+            }
+            if touchLocation.y < y {
+                prepareToJump = true
+            }
         }
         
-        let y = countNormaly()
-        if touchLocation.y < y {
-            prepareToJump = true
-        }
+    }
+    
+    func dropBomb(x: CGFloat, y: CGFloat){
+        var explosionEmitter = SKEmitterNode(fileNamed: "ExplosionFire.sks")
+        var sparkingEmitter = SKEmitterNode(fileNamed: "SparkParticle.sks")
+        
+        bombSprite = SKSpriteNode(texture: bombImage)
+        bombSprite.position.x = x
+        bombSprite.position.y = y
+        bombSprite.xScale = 0.5
+        bombSprite.yScale = 0.5
+        bombSprite!.physicsBody = SKPhysicsBody(rectangleOfSize: bombSprite!.size)
+        bombSprite.physicsBody?.categoryBitMask = PhysicsCategory.Bomb
+        bombSprite.physicsBody?.collisionBitMask = PhysicsCategory.Ground | PhysicsCategory.Bomb
+        bombSprite.zPosition = 1
+        bombSprite.physicsBody?.dynamic = true
+        
+        bombSprite.addChild(sparkingEmitter)
+        let timeDelay = NSTimeInterval(HeroStats.ExplosionTimeDelay)
+        let timeExploding = NSTimeInterval(HeroStats.ExplosionTime)
+        bombSprite.runAction(SKAction.waitForDuration(timeDelay), completion: {
+            sparkingEmitter.removeFromParent()
+            explosionEmitter.position = self.bombSprite.position
+            self.world!.addChild(explosionEmitter)
+            self.bombSprite.removeFromParent()
+            self.bombsCounter--
+            self.world!.runAction(SKAction.waitForDuration(timeExploding), completion: {
+                explosionEmitter.removeFromParent()
+            })
+        })
+        world!.addChild(bombSprite)
         
     }
     
@@ -248,7 +323,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     right = false
                 }
                 
-                heroSprite!.position = CGPoint(x: heroSprite!.position.x - 2, y: heroSprite!.position.y)
+                heroSprite!.position = CGPoint(x: heroSprite!.position.x - 5, y: heroSprite!.position.y)
                 
                 if jump {
                     jump = false
@@ -262,7 +337,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     right = true
                 }
                 
-                heroSprite!.position = CGPoint(x: heroSprite!.position.x + 2, y: heroSprite!.position.y)
+                heroSprite!.position = CGPoint(x: heroSprite!.position.x + 5, y: heroSprite!.position.y)
                 
                 if jump {
                     
@@ -318,8 +393,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //size = imagesize
         ground.anchorPoint = CGPoint(x: 0, y: 0)
-        var groundWidth: Int = Int (ground.frame.width)
-        ground.position = CGPoint(x: groundWidth * groundArray.count, y: 0)
+        var groundWidth: CGFloat = CGFloat (ground.frame.width)
+        groundWidth = groundWidth * ground.xScale
+        ground.position = CGPoint(x: Int(groundWidth) * groundArray.count, y: 0)
         ground.physicsBody = SKPhysicsBody(rectangleOfSize: ground.frame.size, center: CGPoint(x: ground.frame.width / 2, y: ground.frame.height / 2))
         ground.physicsBody?.dynamic = false
         ground.physicsBody?.categoryBitMask = PhysicsCategory.Ground
@@ -334,10 +410,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func createLight(view: SKView){
+        
         light = SKLightNode()
-        light.categoryBitMask = ShadowCategory.All
+        light.categoryBitMask = ShadowCategory.Ground
+        light.ambientColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.7)
+        light.lightColor = UIColor(red: 1.0, green: 1.0, blue: 0.1, alpha: 1)
+        light.shadowColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.7)
         light.enabled = true
-        light.falloff = 0.01
+        light.falloff = 1
         
     }
     
